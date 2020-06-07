17f76f561f1e0f0ecb4ae8e083b655b6ac1cc49d
dayanraj <dayanraj.a@rently.com>
2020-06-08 00:17:23 +0530

    Added duplicate owner / buyer testcase

--- profiles/tests.py
+++ profiles/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
-from .models import Owner
+from .models import Owner, Buyer
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -49,10 +49,23 @@ class OwnerDetailAPIViewTest(APITestCase):
                 "website":"https://test.com",
                 "pan_number":"ABCD1234EF",
                 "avatar":None
-            }
+            },
+            msg='Able to see user detail'
         )
 
 class OwnerCreateAPIViewTest(APITestCase):
+
+    data = {
+            "company_name": "test-company2",
+            "address": "test-address2",
+            "district": "test-district2",
+            "state": "test-state2",
+            "gstin": "test-gstin2",
+            "contact_number": "9876540123",
+            "support_email_id": "test2@test.com",
+            "website": "https://test.com",
+            "pan_number": "ABCD1234EF",
+        }
 
     def setUp(self):
         self.url = reverse('create-owner')
@@ -64,34 +77,34 @@ class OwnerCreateAPIViewTest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
 
     def test_ownercreateview_authenticated(self):
-        data = {
-            "company_name": "test-company2",
-            "address": "test-address2",
-            "district": "test-district2",
-            "state": "test-state2",
-            "gstin": "test-gstin2",
-            "contact_number": "9876540123",
-            "support_email_id": "test2@test.com",
-            "website": "https://test.com",
-            "pan_number": "ABCD1234EF",
-        }
-        response = self.client.post(self.url, data)
+        response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_ownercreateview_authenticated_duplicate(self):
+
+        owner = Owner.objects.create(
+            user = self.user,
+            company_name = "test-company2",
+            address = "test-address2",
+            district = "test-district2",
+            state = "test-state2",
+            gstin = "test-gstin2",
+            contact_number = "9876501234",
+            support_email_id = "test3@test.com",
+            website = "https://test.com",
+            pan_number = "ABCD1234EF",
+        )
+        response = self.client.post(self.url, self.data)
+        # print(response.content)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+                "detail": "You do not have permission to perform this action."
+            }
+        )
 
     def test_ownercreateview_un_authenticated(self):
         self.client.force_authenticate(user=None)
-        data = {
-            "company_name": "test-company2",
-            "address": "test-address2",
-            "district": "test-district2",
-            "state": "test-state2",
-            "gstin": "test-gstin2",
-            "contact_number": "9876540123",
-            "support_email_id": "test2@test.com",
-            "website": "https://test.com",
-            "pan_number": "ABCD1234EF",
-        }
-        response = self.client.post(self.url, data)
+        response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(json.loads(response.content), {
                 'detail': 'Authentication credentials were not provided.'
@@ -99,6 +112,15 @@ class OwnerCreateAPIViewTest(APITestCase):
             )
 
 class BuyerCreateAPIViewTest(APITestCase):
+    
+    data = {
+            "address": "test-address2",
+            "district": "test-district2",
+            "state": "test-state2",
+            "contact_number": "9597200186",
+            "avatar": ""
+        }
+
     def setUp(self):
         self.url = reverse('create-buyer')
         self.user = User.objects.create(email='test-buyer@test.com', name='test-buyer')
@@ -109,28 +131,30 @@ class BuyerCreateAPIViewTest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
 
     def test_buyercreateview_authenticated(self):
-        data = {
-            "address": "test-address2",
-            "district": "test-district2",
-            "state": "test-state2",
-            "gstin": "test-gstin2",
-            "contact_number": "9597200186",
-            "avatar": ""
-        }
-        response = self.client.post(self.url, data)
+        response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_buyercreateview_authenticated_duplicate(self):
+
+        buyer = Buyer.objects.create(
+            user = self.user,
+            address = "test-address2",
+            district = "test-district2",
+            state = "test-state2",
+            contact_number = "9597200186",
+            avatar = ""
+        )
+
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content), {
+                "detail": "You do not have permission to perform this action."
+            }
+        )
 
     def test_buyercreateview_un_authenticated(self):
         self.client.force_authenticate(user=None)
-        data = {
-            "address": "test-address2",
-            "district": "test-district2",
-            "state": "test-state2",
-            "gstin": "test-gstin2",
-            "contact_number": "9597200186",
-            "avatar": ""
-        }
-        response = self.client.post(self.url, data)
+        response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(json.loads(response.content), {
                 'detail': 'Authentication credentials were not provided.'
