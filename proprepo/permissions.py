f31303f08e029791005ce06d3a311b0543e96400
dayanraj <dayanraj.a@rently.com>
2020-07-05 21:46:20 +0530

    Commented unnecessary print statements in permissions file

--- proprepo/permissions.py
+++ proprepo/permissions.py
@@ -9,15 +9,15 @@ User = get_user_model()
 class IsOwnerOfSite(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        print("inside IsOwnerOfSite")
+        # print("inside IsOwnerOfSite")
         user = get_object_or_404(User, email=request.user)
         site_pk = view.kwargs.get('site_pk', None)
-        print(f'Site ID : { site_pk }')
+        # print(f'Site ID : { site_pk }')
         site = get_object_or_404(Site, id=site_pk)
-        print(site.name)
-        print(site.owner_id.company_name)
+        # print(site.name)
+        # print(site.owner_id.company_name)
         
-        print(user.owners.company_name)
+        # print(user.owners.company_name)
         
         if user.owners.company_name == site.owner_id.company_name :
             return True
