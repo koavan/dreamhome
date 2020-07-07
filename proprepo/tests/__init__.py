53438cbd66432f2d82767b5b6d611da38411c3c0
dayanraj <dayanraj.a@rently.com>
2020-07-07 22:31:58 +0530

    Added testcase for property list view

--- proprepo/tests/__init__.py
+++ proprepo/tests/__init__.py
@@ -1,1 +1,2 @@
 from proprepo.tests.test_siteViews import *
+from proprepo.tests.test_propertyViews import *
--- proprepo/tests/test_propertyViews.py
+++ proprepo/tests/test_propertyViews.py
@@ -239,3 +239,99 @@ class PropertyDetailAPIViewTest(APITestCase):
             json.loads(response.content),
             self.data
         )
+
+class PropertyListAPIViewTest(APITestCase):
+    url = reverse('properties-list')
+    def setUp(self):
+        self.user1 = User.objects.create(email='test@test.com', name='test')
+        self.owner1 = Owner.objects.create(user=self.user1, company_name='test-company', address='test-address', 
+            district='test-district', state='test-state',
+            gstin='test-gstin', contact_number='9876543210',
+            support_email_id='test@test.com', website='https://test.com',
+            pan_number='ABCD1234EF', avatar=None)
+        self.site1 = Site.objects.create(
+            name = "test-site",
+            description = "test description",
+            owner_id = self.owner1,
+            located_at = "test locality",
+            latitude = 11.1111,
+            longitude = 12.1212,
+            area_sqft = 100000,
+            area_cents = 229,
+            total_properties = 100,
+            properties_occupied = 50,
+            properties_available = 50,
+            land_rate_sqft = 1300,
+            land_rate_cent = 566800,
+            status = 'AVAILABLE',
+            approved = True,
+            approval_body = "DTCP"
+        )
+
+        self.property1 = Property.objects.create(
+            name = "Plot no : 1",
+            description = "East facing plot",
+            type = "LAND-APPROVED",
+            site_id = self.site1,
+            area_sqft = 760,
+            area_cents = 1.75,
+            facing_direction = "EAST",
+            land_rate_sqft = 1300,
+            land_rate_cent = 566800,
+            status = "AVAILABLE"
+        )
+        
+        self.property2 = Property.objects.create(
+            name = "Plot no : 2",
+            description = "West facing plot",
+            type = "LAND-APPROVED",
+            site_id = self.site1,
+            area_sqft = 760,
+            area_cents = 1.75,
+            facing_direction = "WEST",
+            land_rate_sqft = 1300,
+            land_rate_cent = 566800,
+            status = "AVAILABLE"
+        )
+
+        self.data = [
+            {
+                "id" : 5,
+                "site_id" : self.site1.name,
+                "images" : [],
+                "name" : "Plot no : 1",
+                "description" : "East facing plot",
+                "type" : "LAND-APPROVED",
+                "area_sqft" : 760.0,
+                "area_cents" : 1.75,
+                "facing_direction" : "EAST",
+                "land_rate_sqft" : 1300.0,
+                "land_rate_cent" : 566800.0,
+                "status" : "AVAILABLE"
+            },
+            {
+                "id" : 6,
+                "site_id" : self.site1.name,
+                "images" : [],
+                "name" : "Plot no : 2",
+                "description" : "West facing plot",
+                "type" : "LAND-APPROVED",
+                "area_sqft" : 760.0,
+                "area_cents" : 1.75,
+                "facing_direction" : "WEST",
+                "land_rate_sqft" : 1300.0,
+                "land_rate_cent" : 566800.0,
+                "status" : "AVAILABLE"
+            }
+        ]
+
+    def test_property_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertEqual(
+            json.loads(response.content),
+            self.data
+        )
