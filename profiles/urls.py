7a7a41d2fc87bcbc9d8b19c0df3d8d921b023021
koavan <dayanraj.a@outlook.com>
2020-10-18 15:22:14 +0530

    Added profiles/whoami/ endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import ( OwnerListAPIView, OwnerDetailAPIView, OwnerCreateAPIView,
-                     BuyerCreateAPIView, )
+                     BuyerCreateAPIView, WhoamiAPIView, )
 
 urlpatterns = [
     path('owners/', OwnerListAPIView.as_view(), name='owners-list'),
     path('owners/<int:pk>/', OwnerDetailAPIView.as_view(), name='owner-detail'),
     path('owners/create/', OwnerCreateAPIView.as_view(), name='create-owner'),
     path('buyers/create/', BuyerCreateAPIView.as_view(), name='create-buyer'),
+    path('whoami/', WhoamiAPIView.as_view(), name='who-am-i'),
 ]
