670b57390f061a75401543922473dd1a36087d19
dayanraj <dayanraj.a@rently.com>
2020-10-21 14:41:27 +0530

    Removed duplicate entry for REST_FRAMEWORK in settings.py

--- dreamhome/settings.py
+++ dreamhome/settings.py
@@ -158,7 +158,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : (
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication', )
+        'rest_framework.authentication.TokenAuthentication', ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 AUTH_USER_MODEL = 'profiles.User'
@@ -191,6 +192,3 @@ ACCOUNT_LOGOUT_ON_GET = True
 
 REST_SESSION_LOGIN = True
 
-REST_FRAMEWORK = { 
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' 
-}
