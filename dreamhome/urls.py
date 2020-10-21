9159f6fd50e0cbd2734f678cc55905f431be2237
dayanraj <dayanraj.a@rently.com>
2020-10-21 14:49:36 +0530

    Added description for docs/ url

--- dreamhome/urls.py
+++ dreamhome/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # path('api_auth/', include('rest_framework.urls')),
     path('rest_auth/', include('rest_auth.urls')),
     path('rest_auth/registration/', include('rest_auth.registration.urls')),
-    path('docs/', include_docs_urls(title='dreamhome')),
+    path('docs/', include_docs_urls(title='dreamhome',description='backend of house-owner.in')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
