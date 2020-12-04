d464d2c2357cce9f122419fa98db1dac86af74fc
koavan <dayanraj.a@outlook.com>
2020-12-04 14:08:26 +0530

    Added owner name and site name to property serializer

--- proprepo/serializers.py
+++ proprepo/serializers.py
@@ -11,6 +11,11 @@ class SiteImageSerializer(serializers.ModelSerializer):
 
 class SiteSerializer(serializers.ModelSerializer):
     owner_id = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    owner_name = serializers.SerializerMethodField('get_owner_name')
+
+    def get_owner_name(self, site):
+        return site.owner_id.company_name
     
     images = SiteImageSerializer(many=True, read_only=True)
     class Meta:
@@ -27,12 +32,19 @@ class PropertySerializer(serializers.ModelSerializer):
     site_id = serializers.PrimaryKeyRelatedField(read_only=True)
     images = PropertyImageSerializer(many=True, read_only=True)
 
+    # custom fields
     owner_id = serializers.SerializerMethodField('get_owner_id')
+    owner_name = serializers.SerializerMethodField('get_owner_name')
+    site_name = serializers.SerializerMethodField('get_site_name')
 
     def get_owner_id(self, prop):
-        owner = prop.site_id.owner_id.id
-        print(owner)
-        return owner
+        return prop.site_id.owner_id.id
+
+    def get_owner_name(self, prop):
+        return prop.site_id.owner_id.company_name
+
+    def get_site_name(self, prop):
+        return prop.site_id.name
 
     class Meta:
         model = Property
