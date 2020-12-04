5520e086bb0a1a6d4eb4721beacb2cd9b8aea5c2
koavan <dayanraj.a@outlook.com>
2020-12-04 15:46:54 +0530

    Disabled session auth setting

--- dreamhome/settings.py
+++ dreamhome/settings.py
@@ -157,12 +157,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication', ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    # 'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 AUTH_USER_MODEL = 'profiles.User'
